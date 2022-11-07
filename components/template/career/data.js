412ea8464499ab5b856fbefb68bb8a62a0ab57d4
wslee94 <leemuo123@gmail.com>
2022-11-07 20:36:31 +0900

    career 구현

---
+++ components/template/career/data.js
@@ -0,0 +1,28 @@
+export const careerList = [
+  {
+    id: 'ky',
+    duration: '2013.03 ~ 2019.02',
+    logo: '/assets/img/career/ky.svg',
+    desc: ['융합IT 전공', '평균 학점 3.91 / 4.50'],
+  },
+  {
+    id: 'arrow',
+    logo: '/assets/img/career/arrow.svg',
+  },
+  {
+    id: 'humax',
+    duration: '2019.03 ~ 2022.04',
+    logo: '/assets/img/career/humax.svg',
+    desc: ['웹개발팀, 법률플랫폼TF팀 소속', '변호사 업무 솔루션 FE 개발/유지보수'],
+  },
+  {
+    id: 'arrow',
+    logo: '/assets/img/career/arrow.svg',
+  },
+  {
+    id: 'nexon',
+    duration: '2022.04 ~',
+    logo: '/assets/img/career/nexon.svg',
+    desc: ['인텔리전스랩스 프론트웹개발팀 소속', '게임 관련 FE 개발/유지보수'],
+  },
+];
--- components/template/career/index.js
+++ components/template/career/index.js
@@ -1,5 +1,58 @@
+import { useContext, useEffect, useState } from 'react';
+import Image from 'next/image';
 import style from './style.module.scss';
+import { CurrentPageContext } from '../../../context/CurrentPageContext';
+import { careerList } from './data.js';
 
 export default function Career() {
-  return <div className={`section ${style.section_career}`}>career</div>;
+  const [isAcitve, setIsActive] = useState(false);
+  const { currentPage } = useContext(CurrentPageContext);
+
+  useEffect(() => {
+    if (currentPage === 'career') {
+      setIsActive(true);
+    } else {
+      setIsActive(false);
+    }
+  }, [currentPage]);
+
+  return (
+    <ul className={`section ${style.section_career}`}>
+      {careerList.map((career, index) => {
+        const isLast = careerList.length === index + 1;
+
+        if (career.id === 'arrow') {
+          return (
+            <li
+              key={`${career.id}-${index}`}
+              className={[style.wrapper_item, style.arrow, isAcitve ? style.active : ''].join(' ')}
+            >
+              <Image src="/assets/img/career/arrow.svg" layout="fill" />
+            </li>
+          );
+        }
+
+        return (
+          <li
+            key={career.id}
+            className={[style.wrapper_item, isLast ? style.white : '', isAcitve ? style.active : ''].join(' ')}
+          >
+            <div className={style.wrapper_logo}>
+              <span className={style.text_duration}>{career.duration}</span>
+              <div className={style.logo}>
+                <Image src={career.logo} layout="fill" />
+              </div>
+            </div>
+            <div>
+              {career.desc.map((desc, idx) => (
+                <p className={style.text_desc} key={`${career.id}_${idx}`}>
+                  {desc}
+                </p>
+              ))}
+            </div>
+          </li>
+        );
+      })}
+    </ul>
+  );
 }
