76fa420dc96c20f37f0ea7abe522c3552dffcd23
wslee94 <leemuo123@gmail.com>
2022-11-20 18:54:50 +0900

    gallery 구현

--- components/template/gallery/index.js
+++ components/template/gallery/index.js
@@ -1,5 +1,41 @@
+import Image from 'next/image';
 import style from './style.module.scss';
 
 export default function Gallery() {
-  return <div className={`section ${style.section_gallery}`}>gallery</div>;
+  return (
+    <div className={`section ${style.section_gallery}`}>
+      <div className={style.container_image}>
+        <div className={[style.wrapper_image, style.full].join(' ')}>
+          <Image className={style.image} src="/assets/img/gallery/msw-avatar.gif" layout="fill" />
+        </div>
+        <div className={[style.wrapper_image, style.tree_quaters].join(' ')}>
+          <Image className={style.image} src="/assets/img/gallery/msw-shorts.gif" layout="fill" />
+        </div>
+        <div className={[style.wrapper_image, style.quater].join(' ')}>
+          <Image className={style.image} src="/assets/img/gallery/msw-shorts-detail.gif" layout="fill" />
+        </div>
+        <div className={[style.wrapper_image, style.full].join(' ')}>
+          <Image className={style.image} src="/assets/img/gallery/enter-invitation.png" layout="fill" />
+        </div>
+        <div className={[style.wrapper_image, style.tree_quaters].join(' ')}>
+          <Image className={style.image} src="/assets/img/gallery/enter-wings-dev.gif" layout="fill" />
+        </div>
+        <div className={[style.wrapper_image, style.quater].join(' ')}>
+          <Image className={style.image} src="/assets/img/gallery/enter-wings-dev-mobile.gif" layout="fill" />
+        </div>
+        <div className={[style.wrapper_image, style.half].join(' ')}>
+          <Image className={style.image} src="/assets/img/gallery/enter-wings-partner.png" layout="fill" />
+        </div>
+        <div className={[style.wrapper_image, style.half].join(' ')}>
+          <Image className={style.image} src="/assets/img/gallery/enter-newsletter.png" layout="fill" />
+        </div>
+        <div className={[style.wrapper_image, style.tree_quaters].join(' ')}>
+          <Image className={style.image} src="/assets/img/gallery/enter-wings-pride.gif" layout="fill" />
+        </div>
+        <div className={[style.wrapper_image, style.quater].join(' ')}>
+          <Image className={style.image} src="/assets/img/gallery/enter-wings-pride-mobile.gif" layout="fill" />
+        </div>
+      </div>
+    </div>
+  );
 }
