dc5aa53d0419c0d3f03dfd1ef8afeff67f4cdbbb
wslee94 <leemuo123@gmail.com>
2022-12-04 22:05:06 +0900

    mobile vh 대응 코드 작성

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,26 @@
+import { useEffect } from 'react';
 import '../styles/reset.scss';
 import '../styles/globals.scss';
 import Layout from '../components/layout';
 import { CurrentPageProvider } from '../context/CurrentPageContext';
+import { isDesktop } from 'react-device-detect';
 
 function MyApp({ Component, pageProps }) {
+  const handleSetVh = () => {
+    const vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  };
+
+  useEffect(() => {
+    if (!isDesktop) {
+      handleSetVh();
+      window.addEventListener('resize', handleSetVh);
+
+      return () => {
+        window.removeEventListener('resize', handleSetVh);
+      };
+    }
+  }, []);
   return (
     <CurrentPageProvider>
       <Layout>
