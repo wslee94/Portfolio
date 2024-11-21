6bc51b48103d5e68ca4c4107bb6566193aa1b6dc
wslee94 <leemuo123@gmail.com>
2024-11-21 15:56:43 +0900

    포트폴리오 최신화

--- components/template/gallery/index.js
+++ components/template/gallery/index.js
@@ -6,11 +6,11 @@ export default function Gallery() {
     <div className={`section ${style.section_gallery}`}>
       <div className={style.container_image}>
         <div className={[style.wrapper_image, style.full].join(' ')}>
-          <p className={style.title_image}>MapleStory Worlds - 신규 월드 이벤트</p>
+          <p className={style.title_image}>MapleStory Worlds - 공지사항</p>
           <Image
             alt="project image"
             className={style.image}
-            src="/portfolio/assets/img/gallery/msw-event-new-worlds.gif"
+            src="/portfolio/assets/img/gallery/msw-community-1.png"
             layout="fill"
           />
         </div>
@@ -19,7 +19,16 @@ export default function Gallery() {
           <Image
             alt="project image"
             className={style.image}
-            src="/portfolio/assets/img/gallery/msw-community.png"
+            src="/portfolio/assets/img/gallery/msw-community-2.png"
+            layout="fill"
+          />
+        </div>
+        <div className={[style.wrapper_image, style.full].join(' ')}>
+          <p className={style.title_image}>MapleStory Worlds - 신규 월드 이벤트</p>
+          <Image
+            alt="project image"
+            className={style.image}
+            src="/portfolio/assets/img/gallery/msw-event-new-worlds.gif"
             layout="fill"
           />
         </div>
@@ -131,7 +140,7 @@ export default function Gallery() {
             layout="fill"
           />
         </div>
-        <div className={[style.wrapper_image, style.full].join(' ')}>
+        {/* <div className={[style.wrapper_image, style.full].join(' ')}>
           <p className={style.title_image}>MapleStory Worlds - 친구 초대 이벤트</p>
           <Image
             alt="project image"
@@ -139,7 +148,7 @@ export default function Gallery() {
             src="/portfolio/assets/img/gallery/enter-invitation.png"
             layout="fill"
           />
-        </div>
+        </div> */}
         {/* <div className={[style.wrapper_image, style.half].join(' ')}>
           <p className={style.title_image}>Event Page</p>
           <Image
